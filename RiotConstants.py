35e2478a81ff47cc8542ae189cd19c2ffa4a7a48
meta_system <meta_system@online.de>
2018-02-28 17:26:03 +0100

    Added as necessary

--- RiotConstants.py
+++ RiotConstants.py
@@ -1,9 +1,11 @@
 URL = {
-    'base': 'https://{region}.api.riotgames.com/lol{core}api_key={api_key}',
+    'base': 'https://{region}.api.riotgames.com/lol{core}',
     'summoner_by_name': '/summoner/v3/summoners/by-name/{summonerName}',
     'champion_masteries': '/champion-mastery/v3/champion-masteries/by-summoner/{summonerID}',
-    'champion_name_by_id': '/static-data/v3/champions/{id}',
-    'champions_by_id_list': '/static-data/v3/champions?locale=en_US&dataById=true{sign}'
+    'champion_by_id': '/static-data/v3/champions/{id}',
+    'champions_by_id_list': '/static-data/v3/champions?locale=en_US&dataById=true',
+    'matchlist_by_account_id': '/match/v3/matchlists/by-account/{account_id}',
+    'champion_by_id_dynamic': '/platform/v3/champions/{id}'
 }
 
 
