9917ddae08a5c355422f9fed506d91cb9308ef2d
meta_system <meta_system@online.de>
2018-02-28 17:26:43 +0100

    removed print and id can now be string or int

--- LocalStatic.py
+++ LocalStatic.py
@@ -1,14 +1,11 @@
 class LocalStatic(object):
 
-    def __init__(self, list={}):
-        self.__champion_list = list
-        print(self.__champion_list)
+    def __init__(self, new_list={}):
+        self.champion_list = new_list
+        print(self.champion_list)
 
     def get_champion_by_id(self, id):
-        print ("in by_id")
-        return self.__champion_list["data"][id]
+        return self.champion_list["data"][str(id)]
 
     def get_champion_name_by_id(self, id):
-        print ("in name_by_id")
-
         return self.get_champion_by_id(id)['name']
